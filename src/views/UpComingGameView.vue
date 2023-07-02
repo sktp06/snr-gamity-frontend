9692596fb96cc0b0edeffa08553dda9d6f9f0326
sktp06 <kantaporn_s@cmu.ac.th>
2023-07-03 06:07:54 +0700

    fix: update game card margin

--- src/views/UpComingGameView.vue
+++ src/views/UpComingGameView.vue
@@ -32,7 +32,7 @@
     <div class="flex flex-wrap -mx-4">
       <div v-for="game in filteredGames" :key="game.id" class="w-1/5 px-4">
         <div
-          class="bg-white transform hover:-translate-y-1 hover:scale-105 transition duration-300 shadow-lg rounded-md"
+          class="bg-white transform hover:-translate-y-1 hover:scale-105 transition duration-300 shadow-lg rounded-md ml-2 mr-2"
         >
           <GameCard :game="game" />
         </div>
