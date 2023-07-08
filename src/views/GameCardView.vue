81f571d15878d2dbc0dec8916115eaf0f1840277
POOM <nitipoom_t@cmu.ac.th>
2023-07-09 00:33:55 +0700

    feat:show all game in each genre

--- src/views/GameCardView.vue
+++ src/views/GameCardView.vue
@@ -14,19 +14,18 @@
       <button class="see-more mx-4" @click="toggleGenrePopup(value.genre)">
         {{ expandedGenres.includes(value.genre) ? "See Less" : "See More" }}
       </button>
+
       <div
         v-if="expandedGenres.includes(value.genre)"
         class="genre-popup"
         @click.self="hideGenrePopup"
       >
         <div class="popup-content">
+          <h2 class="genre-title">{{ value.genre }}</h2>
           <button class="close-button" @click="hideGenrePopup">Close</button>
           <div class="game-grid">
             <div
-              v-for="(game, gameIndex) in filteredGames(value.genre).slice(
-                30,
-                60
-              )"
+              v-for="(game, gameIndex) in filteredGames(value.genre)"
               :key="gameIndex"
               class="game-item"
               @click="showGameDetail(game)"
@@ -45,25 +44,6 @@
         <div class="overlay"></div>
         <GameDetail :game="selectedGame" @close="hideGameDetail" />
         <button class="close-button" @click="hideGameDetail">Close</button>
-      </div>
-      <div v-if="showSeeMore" class="see-more-popup">
-        <div class="popup-content">
-          <h2 class="text-2xl font-bold">
-            {{ displayedGenres[seeMoreIndex].genre }}
-          </h2>
-          <div class="game-grid">
-            <div
-              v-for="game in filteredGames(
-                displayedGenres[seeMoreIndex].genre
-              ).slice(30, 60)"
-              :key="game.id"
-              class="game-item"
-            >
-              <img :src="game.cover" alt="Game Cover" class="game-image" />
-            </div>
-          </div>
-          <button class="close-button" @click="hideSeeMorePopup">Close</button>
-        </div>
       </div>
     </div>
   </div>
@@ -232,54 +212,6 @@ export default {
   margin-top: 10px;
   text-align: center;
   color: #fff;
-}
-
-.see-more-popup {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.8);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 9999;
-}
-
-.see-more-popup .popup-content {
-  background-color: #000;
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
-  width: 90%;
-  max-height: 90vh; /* Limit the maximum height of the popup to 90% of the viewport height */
-  overflow-y: auto;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center; /* Center the game items horizontally */
-  align-items: center; /* Center the game items vertically */
-}
-
-.see-more-popup .game-item {
-  width: 200px; /* Adjust the width of each game item based on your needs */
-  height: 200px; /* Set the height equal to the width to make the popup square */
-  margin-right: 20px; /* Adjust the spacing between game items based on your needs */
-  margin-bottom: 20px; /* Adjust the spacing between game items based on your needs */
-}
-
-.see-more-popup .game-image {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  border-radius: 10px;
-}
-
-.see-more-popup .game-title {
-  font-size: 16px;
-  text-align: center;
-  color: #fff;
-  margin-top: 10px;
 }
 
 .close-button {
