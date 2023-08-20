a5beeb76f108170408296a89524c2cda32991dfa
POOM <nitipoom_t@cmu.ac.th>
2023-08-20 23:35:48 +0700

    feat:delete import .ics file and make seacrh fill that can search by game name

--- src/views/TimeManagementView.vue
+++ src/views/TimeManagementView.vue
@@ -1,19 +1,84 @@
 <template>
-  <div class="mb-3">
-    <label for="formFile" class="mb-2 inline-block text-color-red"
-      >Choose file .ics</label
+  <form>
+    <label
+      for="default-search"
+      class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+      >Search</label
     >
-    <input
-      class="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
-      type="file"
-      accept=".ics"
-      id="formFile"
-    />
-  </div>
+    <div class="mt-5 relative">
+      <div
+        class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+      >
+        <svg
+          class="w-4 h-4 text-gray-500 dark:text-gray-400"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 20 20"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+          />
+        </svg>
+      </div>
+      <input
+        v-model="searchQuery"
+        @input="handleSearchInput"
+        type="search"
+        id="default-search"
+        class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Search Games name"
+        required
+      />
+    </div>
+    <ul
+      v-if="searchResults.length > 0 && searchQuery"
+      class="mt-2 border border-gray-300 rounded-lg bg-white overflow-y-auto max-h-48"
+    >
+      <li v-for="result in visibleResults" :key="result.id" class="p-2">
+        {{ result.name }}
+      </li>
+    </ul>
+  </form>
 </template>
 
 <script>
-export default {};
+import gameService from "@/services/gameService";
+
+export default {
+  data() {
+    return {
+      searchQuery: "",
+      searchResults: [],
+      visibleResults: [],
+    };
+  },
+  methods: {
+    async handleSearchInput() {
+      try {
+        if (this.searchQuery === "") {
+          this.searchResults = [];
+          this.visibleResults = [];
+          return;
+        }
+
+        const games = await gameService.getGames();
+        this.searchResults = games.filter((game) =>
+          game.name.toLowerCase().includes(this.searchQuery.toLowerCase())
+        );
+
+        // Limit the visible results to a maximum of 20
+        this.visibleResults = this.searchResults.slice(0, 20);
+      } catch (error) {
+        console.error("Error handling search input:", error);
+      }
+    },
+  },
+};
 </script>
 
 <style></style>
