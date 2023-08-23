4aef745206cdfd4aca38b778fb3be678058b3bfb
POOM <nitipoom_t@cmu.ac.th>
2023-08-24 02:06:56 +0700

    feat: when select game from search it will show detail game time and calendar to book

--- src/views/TimeManagementView.vue
+++ src/views/TimeManagementView.vue
@@ -1,67 +1,86 @@
 <template>
-  <form ref="searchForm">
-    <label
-      for="default-search"
-      class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-    >
-      Search
-    </label>
-    <div class="mt-5 relative">
-      <div
-        class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+  <div class="bg-zinc-900 min-h-screen">
+    <form ref="searchForm">
+      <label
+        for="default-search"
+        class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-black"
       >
-        <svg
-          class="w-4 h-4 text-gray-500 dark:text-gray-400"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 20 20"
+        Search
+      </label>
+      <div class="relative">
+        <div
+          class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
         >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-          />
-        </svg>
+          <svg
+            class="w-4 h-4 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </div>
+        <input
+          v-model="searchQuery"
+          @input="handleSearchInput"
+          @focus="handleInputFocus"
+          type="search"
+          id="default-search"
+          class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Search Games name"
+          required
+        />
       </div>
-      <input
-        v-model="searchQuery"
-        @input="handleSearchInput"
-        type="search"
-        id="default-search"
-        class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="Search Games name"
-        required
-      />
-    </div>
-    <transition name="fade">
-      <ul
-        v-if="visibleResults.length > 0"
-        class="mt-2 border border-gray-300 rounded-lg bg-white overflow-y-auto max-h-48 shadow"
-      >
-        <li
-          v-for="result in sortedVisibleResults"
-          :key="result.id"
-          class="p-3 flex items-center space-x-4 hover:bg-gray-100 cursor-pointer"
-          @click="handleGameClick(result.name)"
+      <transition name="fade">
+        <ul
+          v-if="visibleResults.length > 0"
+          class="mt-2 border border-gray-300 rounded-lg bg-white overflow-y-auto max-h-48 shadow"
         >
-          <img
-            :src="result.cover"
-            alt="Game Cover"
-            class="w-12 h-12 rounded-lg object-cover"
-          />
-          <div class="flex-1">
-            <p class="text-sm font-semibold capitalize">{{ result.name }}</p>
-            <p class="text-xs text-gray-600">
-              Rating: {{ result.aggregated_rating }}
-            </p>
-          </div>
-        </li>
-      </ul>
-    </transition>
-  </form>
+          <li
+            v-for="result in sortedVisibleResults"
+            :key="result.id"
+            class="p-3 flex items-center space-x-4 hover:bg-gray-100 cursor-pointer"
+            @click="handleGameClick(result.name)"
+          >
+            <img
+              :src="result.cover"
+              alt="Game Cover"
+              class="w-12 h-12 rounded-lg object-cover"
+            />
+            <div class="flex-1">
+              <p class="text-sm font-semibold capitalize">{{ result.name }}</p>
+              <p class="text-xs text-gray-600">
+                Rating: {{ result.aggregated_rating }}
+              </p>
+            </div>
+          </li>
+        </ul>
+      </transition>
+    </form>
+    <div v-if="selectedGame" class="mt-4 p-4 bg-white rounded-lg shadow">
+      <h3 class="text-lg font-semibold capitalize">{{ selectedGame.name }}</h3>
+      <p>Main Story: {{ selectedGame.main_story }}</p>
+      <p>Main Extra: {{ selectedGame.main_extra }}</p>
+      <p>Completionist: {{ selectedGame.completionist }}</p>
+      <div class="mt-4">
+        <label class="block text-sm font-medium text-gray-700"
+          >Select Play Date</label
+        >
+        <input
+          type="date"
+          v-model="playDate"
+          class="mt-1 block w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -73,6 +92,8 @@ export default {
       searchQuery: "",
       searchResults: [],
       visibleResults: [],
+      selectedGame: null,
+      playDate: "",
     };
   },
   computed: {
@@ -104,19 +125,16 @@ export default {
     },
     handleGameClick(gameName) {
       this.searchQuery = gameName;
-      this.visibleResults = []; // Clear search results when clicking a game name
+      this.visibleResults = [];
+      this.selectedGame = this.searchResults.find(
+        (game) => game.name === gameName
+      );
     },
-    handleOutsideClick(event) {
-      if (!this.$refs.searchForm.contains(event.target)) {
-        this.visibleResults = [];
+    handleInputFocus() {
+      if (this.searchQuery && this.searchResults.length > 0) {
+        this.visibleResults = this.searchResults.slice(0, 20);
       }
     },
-  },
-  mounted() {
-    document.addEventListener("click", this.handleOutsideClick);
-  },
-  beforeUnmount() {
-    document.removeEventListener("click", this.handleOutsideClick);
   },
 };
 </script>
